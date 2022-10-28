7108890fd42555a69decb3bd974bf25097576211
Ilya Ermolin <tomixlove@gmail.com>
2022-10-28 17:45:14 +0300

    Try to add 'classToggle' into Component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/app';
 
-
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App/>
