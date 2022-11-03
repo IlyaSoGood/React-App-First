1e755249862e7f4700886d20ec944f2e64ad0497
Ilya Ermolin <tomixlove@gmail.com>
2022-11-03 18:08:50 +0300

    Удаление элемента интерфейса удаляет элемент из data при помощи property drill. Для этого переделываем app.js на классовый компонент для изменения данных как Состояние компонента

--- src/index.js
+++ src/index.js
@@ -7,16 +7,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App/>
 );
-
-let user = {
-    firstName: "Вася",
-    sayHi() {
-      console.log(`Привет, ${this.firstName}!`);
-    }
-  };
-  
-  let sayHi = user.sayHi.bind(user); // (*)
-  
-  sayHi(); // Привет, Вася!
-  
-  setTimeout(sayHi, 3000);
