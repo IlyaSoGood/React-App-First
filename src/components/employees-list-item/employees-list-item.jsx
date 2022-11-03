0248e6714b5ff4c7820fbb92bf43e2c158f5f059
Ilya Ermolin <tomixlove@gmail.com>
2022-11-03 14:06:51 +0300

    Lesson 133. Use react event listeners and setState

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -1,32 +1,55 @@
+import { Component } from 'react';
 import './employees-list-item.css'
 
-const EmployeesListItem = ({name, salary, increase}) => {
-    // const addClass;
-    if (increase) {
-        const addClass = 'inrease';
-    } else {
-        const addClass= '';
+class EmployeesListItem extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            rise: false
+        }
     }
-    // increase ? addClass='increase' : '';
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+    onRise = () => {
+        this.setState(({rise}) => ({
+            rise: !rise
+        }))
+    }
 
-    return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
-
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    render() {
+        const {name, salary} = this.props;
+        const {increase, rise} = this.state;
+        let classNames = "list-group-item d-flex justify-content-between"
+        if (increase) {classNames += ' increase'}
+        if (rise) {classNames += ' like'}
+    
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label"
+                      onClick={this.onRise}
+                >{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button type="button"
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App/>
 );
+
+let user = {
+    firstName: "Вася",
+    sayHi() {
+      console.log(`Привет, ${this.firstName}!`);
+    }
+  };
+  
+  let sayHi = user.sayHi.bind(user); // (*)
+  
+  sayHi(); // Привет, Вася!
+  
+  setTimeout(sayHi, 3000);
